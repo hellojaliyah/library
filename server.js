e331e79c752e5f4c24a58353f455a4df636b2870
Jaliyah Harrison <J@unetixs.local>
2020-02-18 17:01:59 -0500

    Initial Author Routes

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+const expressLayouts = require("express-ejs-layouts")
+const bodyParser = require('body-parser')
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+require('dotenv/config')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + "/views")
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB, { useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.on('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
+
+app.listen(process.env.PORT || 3000)
