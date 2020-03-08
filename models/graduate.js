88b8f0a81348bcd3d976c0bece5124ffff176fae
Jaliyah <jaliyahdharrison@gmail.com>
2020-03-07 21:40:50 -0500

    finished graduates route

--- models/graduate.js
+++ models/graduate.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const graduateSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
     }
+})
+
+graduateSchema.pre('remove', function(next) {
+    Book.find({ graduate: this.id }, (err, books) => {
+        if(err) {
+            next(err)
+        }else if (books.length > 0) {
+            next(new Error('This graduate has books still'))
+        } else {
+            next()
+        }
+    })
 })
 
 module.exports = mongoose.model('Graduate', graduateSchema)
--- routes/graduates.js
+++ routes/graduates.js
@@ -1,6 +1,7 @@
 const express= require('express')
 const router = express.Router();
 const Graduate = require('../models/graduate')
+const Book = require('../models/book')
 
 //All authors
 router.get('/', async (req, res) =>{
@@ -22,8 +23,7 @@ router.get('/', async (req, res) =>{
 
 //New author route
 router.get('/new', (req, res) =>{
-    res.render('graduates/new', 
-    {graduate: new Graduate()})
+    res.render('graduates/new', {graduate: new Graduate()})
 })
 
 //Create author
@@ -33,14 +33,69 @@ router.post('/', async (req, res) =>{
     })
     try {
         const newGraduate = await graduate.save()
-        // res.redirect(`authors/${newAuthor.id}`)
-            res.redirect(`graduates`)
+        res.redirect(`graduates/${newGraduate.id}`)
     } catch {
     res.render('graduates/new', {
     graduate: graduate,
     errorMessage: 'Error creating profile'
                     })
     }
+})
+
+router.get('/:id', async (req, res) =>{
+    try {
+        const graduate = await Graduate.findById(req.params.id)
+        const books = await Book.find({ graduate: graduate.id }).limit(6).exec()
+        res.render('graduates/show', {
+        graduate: graduate,
+        booksByGraduate: books
+        })
+    } catch (err) {
+        res.redirect('/')
+    }
+})
+
+router.get('/:id/edit', async (req, res) => {
+try {
+    const graduate = await Graduate.findById(req.params.id)
+    res.render('graduates/edit', {graduate: graduate })
+} catch {
+res.redirect('/graduates')
+}
+})
+
+router.put('/:id', async (req, res) => {
+    let graduate 
+    try {
+        graduate = await Graduate.findById(req.params.id)
+        graduate.name = req.body.name
+        await graduate.save()
+        res.redirect(`/graduates/${graduate.id}`)
+    } catch {
+        if(graduate == null) {
+            res.redirect('/')
+        } else {
+    res.render('graduates/edit', {
+    graduate: graduate,
+    errorMessage: 'Error updating profile'
+            })
+        }
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+let graduate 
+    try {
+        graduate = await Graduate.findById(req.params.id)
+        await graduate.remove()
+        res.redirect(`/graduates`)
+    } catch {
+        if(graduate == null) {
+            res.redirect('/')
+        } else {
+    res.redirect(`/graduates/${graduate.id}`)
+        }
+     }
 })
 
 module.exports = router
